cdfdf3c56426da5c5c175ba959a2a1550fd6fac8
Mateeb Alharbi <63017846+Mateeb11@users.noreply.github.com>
2024-09-16 22:24:56 +0300

    Configuring Firebase + Testing read and write

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,65 @@
-import { Component } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+} from 'firebase/firestore';
+
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'one-identity-clone';
+
+  // Your web app's Firebase configuration
+  firebaseConfig = {
+    apiKey: 'AIzaSyBWEOvUscWifCIVFs8dw96q0C4t1Hb95KY',
+    authDomain: 'one-identity-clone.firebaseapp.com',
+    projectId: 'one-identity-clone',
+    storageBucket: 'one-identity-clone.appspot.com',
+    messagingSenderId: '603658530826',
+    appId: '1:603658530826:web:7e9de1bc7f44cce914eff3',
+  };
+
+  // Initialize Firebase
+  app = initializeApp(this.firebaseConfig);
+  db = getFirestore(this.app);
+
+  dbValue = '';
+
+  async getData() {
+    const docRef = doc(this.db, 'Test', 'TestName');
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log('Document data:', docSnap.data());
+      this.dbValue = docSnap.data()['Name'];
+    } else {
+      // docSnap.data() will be undefined in this case
+      console.log('No such document!');
+    }
+  }
+
+  @ViewChild('writeInputValue') writeInputValue!: ElementRef;
+
+  async setData() {
+    const citiesRef = collection(this.db, 'Test');
+
+    await setDoc(doc(citiesRef, 'TestName'), {
+      Name: this.writeInputValue.nativeElement.value,
+    });
+
+    console.log(`Write value = ${this.writeInputValue.nativeElement.value}`);
+  }
 }
