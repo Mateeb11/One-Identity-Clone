173075c17cecd0692d40bab8b2abc47faaa23e58
Jawahirah Alsafari <Jawahirahdev@gmail.com>
2024-09-17 10:59:43 +0300

    restructuring the code

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: 'AIzaSyBWEOvUscWifCIVFs8dw96q0C4t1Hb95KY',
+        authDomain: 'one-identity-clone.firebaseapp.com',
+        projectId: 'one-identity-clone',
+        storageBucket: 'one-identity-clone.appspot.com',
+        messagingSenderId: '603658530826',
+        appId: '1:603658530826:web:7e9de1bc7f44cce914eff3',
+      },
+};
