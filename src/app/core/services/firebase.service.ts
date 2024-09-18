fe85e75e2aec9a350e656c527a56d0018d910e64
Mateeb Alharbi <63017846+Mateeb11@users.noreply.github.com>
2024-09-18 13:14:06 +0300

    Set users not excited in CSV file inactive

--- src/app/core/services/firebase.service.ts
+++ src/app/core/services/firebase.service.ts
@@ -8,6 +8,7 @@ import {
   collection,
   query,
   getDocs,
+  updateDoc,
 } from 'firebase/firestore';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Papa } from 'ngx-papaparse';
@@ -52,19 +53,36 @@ export class FirebaseService {
 
   public csvConector(file: any): void {
     this.papa.parse(file, {
+      skipEmptyLines: true,
       complete: async (result: any) => {
         const mailBoxRef = collection(this.db, 'MailBox');
         const adsRef = collection(this.db, 'ADSAccount');
 
+        // Make all documents "isActive" = FALSE
+        const mailBoxSnapshot = await getDocs(query(mailBoxRef));
+        mailBoxSnapshot.forEach(async (document) => {
+          await updateDoc(doc(mailBoxRef, document.id), {
+            isActive: false,
+          });
+        });
+
+        const adsSnapshot = await getDocs(query(adsRef));
+        adsSnapshot.forEach(async (document) => {
+          await updateDoc(doc(adsRef, document.id), {
+            isActive: false,
+          });
+        });
+
+        // Add or update firebase documents with csv file data
         result.data.slice(1).forEach(async (user: any) => {
           await setDoc(doc(mailBoxRef, user[0]), {
-            isActive: user[4] || true,
+            isActive: true,
             isCompanyEmployee: user[3] || true,
             quota: user[3] == 'TRUE' ? 5 : 2,
           });
 
           await setDoc(doc(adsRef, user[0]), {
-            isActive: user[4] || true,
+            isActive: true,
             email: user[2],
             fullName: user[1],
           });
